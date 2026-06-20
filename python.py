cd947b85bee52e342555fd4e5294247509408269
sadikh78 <sadikhshaik7866@gmail.com>
2026-06-20 16:27:23 +0530

    python program

---
+++ python.py
@@ -0,0 +1,40 @@
+name = "sadikh"
+age = 22
+price = 25.44
+old = False
+a = None
+print(type(name))
+print(type(age))
+print(type(price))
+print(type(old))
+print(type(a))
+
+
+name = "sadikh"
+age = 22
+price = 250
+age2=age
+print(name)
+print(age)
+print(price)
+print("my name is ",name)
+print("my age is",age)
+print("my name is ",name ,"my age is",age)
+print(age2)
+
+
+A,B = 2,3
+a = "2"
+c = 4
+d = 2.5
+txt = "@"
+print(2*txt*3)
+print((a+txt)*B)
+print(A+B*c)
+print(A*d)
+
+
+a = 10
+b = 30
+sum = a+b
+print(sum)
